3dcdf926e72c404c783de469e113283d5a63bb00
dileep <valluridileep999@gmail.com>
2022-04-14 14:45:22 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import ClassTimer from './components/ClassTimer';
+import Focusinput from './components/Focusinput';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Focusinput/> */}
+     <ClassTimer/>
+    </div>
+  );
+}
+
+export default App;
